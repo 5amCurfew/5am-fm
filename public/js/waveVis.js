8ee2596b4168766edceb2ace40970c97a78cf2a7
Samuel Knight <samueltobyknight@gmail.com>
2021-08-03 22:55:28 +0100

    minimal player

---
+++ public/js/waveVis.js
@@ -0,0 +1,6 @@
+let wave = new Wave();
+options = {
+    type:"web", 
+    colors: ["black", "black", "white"]
+};
+wave.fromElement("player", "visualCanvas", options);
--- public/waveVis.js
+++
@@ -1,9 +0,0 @@
-var audio = document.getElementById("mp3");
-audio.volume = 0.5;
-
-let wave = new Wave();
-options = {
-    type:"web", 
-    colors: ["black", "black", "white"]
-};
-wave.fromElement("mp3", "visualCanvas", options);
